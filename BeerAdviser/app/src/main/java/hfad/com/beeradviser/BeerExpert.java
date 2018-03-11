b9e50c34fe2967a12d31dfe83338a4a3d7033c21
felipeaugustox <augustto.felipe@hotmail.com>
2018-03-11 17:19:15 -0300

    second chapter

---
+++ BeerAdviser/app/src/main/java/hfad/com/beeradviser/BeerExpert.java
@@ -0,0 +1,23 @@
+package hfad.com.beeradviser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by augus on 3/11/2018.
+ */
+
+public class BeerExpert {
+    List<String> getBrands(String color){
+        List<String> brands = new ArrayList<String>();
+
+        if(color.equals("amber")){
+            brands.add("Jack Amber");
+            brands.add("Red Moose");
+        }else{
+            brands.add("Jail Pale Ale");
+            brands.add("Gout Stout");
+        }
+        return brands;
+    }
+}
--- BeerAdviser/app/src/main/java/hfad/com/beeradviser/FindBeerActivity.java
+++ BeerAdviser/app/src/main/java/hfad/com/beeradviser/FindBeerActivity.java
@@ -2,12 +2,41 @@ package hfad.com.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class FindBeerActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    // call when the user clicks the button
+    public void onClickFindBeer(View view){
+
+        // get the reference to the text view
+        TextView brands = (TextView) findViewById(R.id.brands);
+        // get the reference to the spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        // get the selected item on the spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        // get recomendations for beer expert class
+        List<String> brandsList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for(String brand : brandsList){
+            brandsFormatted.append(brand).append('\n');
+        }
+
+        // display the beers
+        brands.setText(brandsFormatted);
+
     }
 }
