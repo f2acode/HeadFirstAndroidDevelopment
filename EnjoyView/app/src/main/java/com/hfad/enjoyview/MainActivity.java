6aad6b09a377602c874e1df1a5e425d6427ce8d8
felipeaugustox <augustto.felipe@hotmail.com>
2018-03-25 02:30:16 -0300

    wip chapter 5

--- EnjoyView/app/src/main/java/com/hfad/enjoyview/MainActivity.java
+++ EnjoyView/app/src/main/java/com/hfad/enjoyview/MainActivity.java
@@ -2,13 +2,20 @@ package com.hfad.enjoyview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
     }
+
 }
